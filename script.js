560dece8461e1a15a83fde57b3b13df56e9a6c89
Kevin Smith <123609428+ksmith552@users.noreply.github.com>
2023-03-14 00:05:55 -0500

    reworked function
    refactored playRound function to be more readble and shorter
    
    created function
    
    created game() function that will loop through the playRound() function
    5 times. This function alert the winner
    
    created function
    
    created playAgain() which confirms if the user wants to play again,
    if yes the integers are reset and game() is ran again

--- script.js
+++ script.js
@@ -7,20 +7,74 @@ function getComputerChoice() {
     return choiceArray
 }
 
+function getPlayerChoice() {
+    prompt("make your choice");
+}
+
+function playAgain() {
+    if (confirm("do you want to play again")) {
+        userScore = parseInt(0);
+        pcScore = parseInt(0);
+        game()
+    } else {
+        alert("weenie hut jr")
+    }
+    
+}
+
+
+let userScore = parseInt(0);
+let pcScore = parseInt(0);
+
 //play a round** take player in put - rock papaer scissors, store user input. computer input will be the get computer choice function. return the winner//
 
-
-
-function playRound(computerSelection, playerSelection,) {
+//calling the game function will start a new round and start the game over
+function game() {
+    
+    for (let i = 0; i < 5; i++) {
     computerSelection = getComputerChoice();
     playerSelection = prompt("make your selection");
-    if (playerSelection === computerSelection) {
+    console.log(playRound(computerSelection, playerSelection))
+    }
+
+   if (userScore === pcScore) {
+    alert("STALEMATE")
+   } else if ( userScore > pcScore) {
+    alert("you win!!!!")
+   } else {
+    alert("you lose")
+   }
+   
+   playAgain()
+    
+}
+
+    
+/* fix the strings to make more sense and wordy*/
+
+/*at the end of 5 rounds console.log the winner*/
+
+
+
+
+/*function playRound(computerSelection, playerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("make your selection");
+    let userScore = 0;
+    let pcScore = 0;*/
+    
+    
+    /*if (playerSelection === computerSelection) {
         return ("draw")
     } else if (playerSelection === 'Rock' && computerSelection === "Scissors"){ 
-        return ("you win, Rock beats Scissors!");
+        userScore++
+        return score + ("you win, Rock beats Scissors!");
+        console.log(`your current score is' ${userScore} 'and the computer score is' ${pcScore}`)
     } else if (playerSelection === 'Paper' && computerSelection === "Rock"){ 
+        userScore++
         return ("you win, Paper beats Rock!");
     } else if (playerSelection === 'Scissors' && computerSelection === "Paper"){ 
+        userScore++
         return ("you win, Scissors beats Paper!");    
     } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
         return ("you lose, scissors beat paper")
@@ -30,9 +84,51 @@ function playRound(computerSelection, playerSelection,) {
         return ("you lose, Paper beats Rock")
     } else {
         return ("enter valid combo");
-    }
-    // THIS MIGHT BE WORKING WELL ENOUGH NOWthis is somewhat working, but theres an issue with the final alert, even if Rock is entered correctly its not evaluating the expression as it should. need to put something in here after that iif "computer selection doesnt have these parameters it automatically wins" shit aint working brother, prolly need to rewrite this//
-    }
+    }*/
+    
+    // THIS MIGHT BE WORKING WELL ENhOUGH NOWthis is somewhat working, but theres an issue with the final alert, even if Rock is entered correctly its not evaluating the expression as it should. need to put something in here after that iif "computer selection doesnt have these parameters it automatically wins" shit aint working brother, prolly need to rewrite this//
+    
+
+    function playRound(computerSelection, playerSelection) {
+        
+    /* need to add the method that makes player selection case insensitive?*/
+
+        if (playerSelection === computerSelection) {
+            console.log(`you have ${userScore} points, the computer has ${pcScore}`)
+            return ("draw")
+        } if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            userScore++
+            console.log(`you have ${userScore} points, the computer has ${pcScore}`)
+            return ("you win, Rock beats Scissors!")
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            userScore++
+            console.log(`you have ${userScore} points, the computer has ${pcScore}`)
+            return ("you win, Paper beats Rock")
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            userScore++
+            console.log(`you have ${userScore} points, the computer has ${pcScore}`)
+            return ("you win, Scissors beats Paper!")
+        } else  {
+            pcScore++
+            console.log(`you have ${userScore} points, the computer has ${pcScore}`)
+            return ("you lose")
+        }
+           
+        
+        }
+    
+        
+
+        
+
+        
+    
+        
+    
+            
+
+
+       
 
 
 
